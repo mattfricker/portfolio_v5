1913d36a7dd2c215cc5c1c15d771e8367d713297
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-02-26 10:44:35 -0500

    update project

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,12 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Portfolio from '../containers/Portfolio'
 
-
 const App = () => (
-  <div>
-    <Header />
-    <Portfolio />
-  </div>
+    <div>
+        <Header />
+        <Portfolio />
+    </div>
 )
 
 export default App
--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import AppBar from 'material-ui/lib/app-bar'
 import Paper from 'material-ui/lib/paper'
-import Github from 'material-ui/lib/svg-icons/';
-import Colors from 'material-ui/lib/styles/colors';
 
-var paperStyle = {background: 'red', height: '90vh'}
+var paperStyle = {background: '#00bcd4', height: '50vh', color: 'white'};
 
 const Header = () => (
     <div>
-        <AppBar 
-            showMenuIconButton={false}
-        />
-        <Paper style={paperStyle}>
-        
-        </Paper>
+    <AppBar 
+        showMenuIconButton={false}
+    />
+
+    <Paper style={paperStyle}>
+                    
+    </Paper>
     </div>
 )
 

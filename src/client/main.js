12d79fc40a8646b09ce7949bec35a46aa428f9b8
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-02-29 13:21:01 -0500

    add slide left and slide right css transitions

--- src/client/main.js
+++ src/client/main.js
@@ -14,7 +14,7 @@ var initialPortfolio = [
         title: "CSTK (working version)",
         link: 'http://www.targetedkilling.org:3030',
         description: "This is the working version of the CSTK website, built with Angularjs, Express, and MongoDB",
-        image: "http://matthewfricker.com/img/cstk.png",
+        image: "//c2.staticflickr.com/2/1585/25266435441_bb4e08525b_b.jpg",
         status: "Completed in 2015"
     },
     {
@@ -22,7 +22,7 @@ var initialPortfolio = [
         title: "CSTK",
         link: 'http://www.targetedkilling.org:3030',
         description: "This is the current production version of CSTK buil in Q1 2015, it uses Angular, Express, and MySQLB",
-        image: "http://matthewfricker.com/img/cstk.png",
+        image: "//c2.staticflickr.com/2/1479/25359493365_329e48fdb1_c.jpg",
         status: "In Active Development"
     },
 ]

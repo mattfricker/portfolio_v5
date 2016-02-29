c307fc33fbea2557d8fe008c6572607b1228cccb
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-02-29 13:35:21 -0500

    add lazy load to portfolio items

--- src/client/components/PortfolioItem.js
+++ src/client/components/PortfolioItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styleable from 'react-styleable'
+import LazyLoad from 'react-lazy-load';
 
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
@@ -11,20 +12,22 @@ import CardText from 'material-ui/lib/card/card-text';
 import css from '../styles/components/portfolioItem.css';
 
 const PortfolioItem = ({css, title, image, link, description, status}) => (
-  <Card className={css.root}>
-    <CardMedia
-      overlay={<CardTitle title={title} subtitle={status} />}
-    >
-      <img src={image} />
-    </CardMedia>
-    <CardText>
-        {description}
-    </CardText>
-    <CardActions>
-      <FlatButton label="Visit Page" />
-      <FlatButton label="See GitHub Repo" />
-    </CardActions>
-  </Card>
+  <LazyLoad offsetVertical={80} className={css.root}>
+    <Card className={css.card}>
+        <CardMedia
+        overlay={<CardTitle title={title} subtitle={status} />}
+        >
+        <img src={image} />
+        </CardMedia>
+        <CardText>
+            {description}
+        </CardText>
+        <CardActions>
+        <FlatButton label="Visit Page" />
+        <FlatButton label="See GitHub Repo" />
+        </CardActions>
+    </Card>
+  </LazyLoad>
 )
 
 export default styleable(css)(PortfolioItem)

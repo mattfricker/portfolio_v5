8e77a5d1307174c5ac753aa58c0512dab4ecc2e9
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-03-01 11:03:32 -0500

    add github icon to header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -3,8 +3,10 @@ import styleable from 'react-styleable'
 
 import AppBar from 'material-ui/lib/app-bar'
 import RaisedButton from 'material-ui/lib/raised-button';
+import SvgIcon from 'material-ui/lib/svg-icon';
 
 import logo from '../../../assets/images/logo.png'
+import GitHubIcon from '../../../assets/SVG/github.svg'
 import css from '../styles/components/header.css';
 
 const Header = ({ css }) => (
@@ -12,6 +14,13 @@ const Header = ({ css }) => (
         <AppBar 
             showMenuIconButton={false}
             title={<img className={css.logo} src={logo}></img>}
+            iconElementRight={
+                <a href="https://github.com/mattfricker">
+                    <SvgIcon>
+                            <img src={GitHubIcon}></img> 
+                    </SvgIcon>
+                </a>
+            }
         />
         <div className={css.cta}>
             <div className={css.actionHolder}>

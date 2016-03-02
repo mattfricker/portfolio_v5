76f605714047efe27f17a5982921cb50d6ba4a1e
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-03-02 15:39:47 -0500

    place portfolio info in external file

--- src/client/main.js
+++ src/client/main.js
@@ -6,26 +6,9 @@ import { createStore } from 'redux'
 import homepageApp from './reducers'
 import App from './components/App'
 
-import css from './styles/app.css'
+import initialPortfolio from '../data/portfolio.js'
 
-var initialPortfolio = [
-    {
-        id: 1,
-        title: "CSTK (working version)",
-        link: 'http://www.targetedkilling.org:3030',
-        description: "This is the working version of the CSTK website, built with Angularjs, Express, and MongoDB",
-        image: "//c2.staticflickr.com/2/1585/25266435441_bb4e08525b_b.jpg",
-        status: "Completed in 2015"
-    },
-    {
-        id: 2,
-        title: "CSTK",
-        link: 'http://www.targetedkilling.org:3030',
-        description: "This is the current production version of CSTK buil in Q1 2015, it uses Angular, Express, and MySQLB",
-        image: "//c2.staticflickr.com/2/1479/25359493365_329e48fdb1_c.jpg",
-        status: "In Active Development"
-    },
-]
+import css from './styles/app.css'
 
 let store = createStore(homepageApp, {portfolio: initialPortfolio});
 

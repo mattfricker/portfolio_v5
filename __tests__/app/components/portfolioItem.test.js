15b30497aabb2cc3abccfb7f98156800488b576d
Matthew S. Fricker <matt.s.sfricker@gmail.com>
2016-03-08 10:35:42 -0500

    update basic test

--- __tests__/app/components/portfolioItem.test.js
+++ __tests__/app/components/portfolioItem.test.js
@@ -9,7 +9,7 @@ import CheckboxWithLabel from '../../../src/client/components/PortfolioItem';
 describe('Tests for the portfolioItem component', function() {
    describe('Has correct direction of sliding (left or right)', function() {
       it('First component should slide in from the left', function() {
-          
+          expect(true).toBe(true);
       });
    });
 });
